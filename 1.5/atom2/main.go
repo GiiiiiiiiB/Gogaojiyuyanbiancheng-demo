126c30e092724dd4e019a00a71a42366d91a19b5
Wangzhipe <50153321@qq.com>
2022-05-27 14:15:56 +0800

    add atom2

--- 1.5/atom2/main.go
+++ 1.5/atom2/main.go
@@ -3,21 +3,16 @@ package main
 import (
 	"fmt"
 	"sync"
+	"sync/atomic"
 )
 
-type total struct {
-	mu sync.Mutex
-	int
-}
-
-var t = new(total)
+var total uint64
 
 func worker(wg *sync.WaitGroup) {
 	defer wg.Done()
-	for i := 0; i < 200; i++ {
-		t.mu.Lock()
-		t.int += 1
-		t.mu.Unlock()
+	var i uint64
+	for i = 0; i < 100; i++ {
+		atomic.AddUint64(&total, i)
 	}
 }
 
@@ -26,6 +21,7 @@ func main() {
 	wg.Add(2)
 	go worker(&wg)
 	go worker(&wg)
+	fmt.Println(total)
 	wg.Wait()
-	fmt.Println(t.int)
+	fmt.Println(total)
 }
